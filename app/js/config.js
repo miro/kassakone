3be8203db3318000c4d03861f0d72bc9528fc5de
Jukka Tupamaki <jukka.tupamaki@futurice.com>
2014-05-03 15:11:31 +0300

    Fixed structure

---
+++ app/js/config.js
@@ -0,0 +1,7 @@
+require.config({
+    paths: {
+        underscore: 'bower_components/underscore/underscore',
+        backbone: 'bower_components/backbone/backbone',
+        jquery: 'bower_components/jquery/dist/jquery'
+    }
+});
--- app/lib/config.js
+++
@@ -1,7 +0,0 @@
-require.config({
-    paths: {
-        underscore: '/bower_components/underscore/underscore',
-        backbone: '/bower_components/backbone/backbone',
-        jquery: '/bower_components/jquery/dist/jquery'
-    }
-});
