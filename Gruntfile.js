8392dabc4da036e02d6ce041bbd35ee7b603b26c
Miro Nieminen <miro.nieminen@futurice.com>
2014-05-06 20:51:28 +0300

    Add grunt-sass to project

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,15 +10,30 @@ module.exports = function(grunt) {
                 ext: '.js'
             }
         },
+
+        sass: {
+            compile: {        
+                files: {
+                    'app/css/client.css': 'app/scss/styles.scss'
+                }
+            }
+        },
+
         watch: {
             react: {
                 files: 'app/jsx/**/*.jsx',
                 tasks: ['react']
+            },
+
+            scss: {
+                files: 'app/scss/*.scss',
+                tasks: ['sass']
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-react');
+    grunt.loadNpmTasks('grunt-sass');
 
 };
