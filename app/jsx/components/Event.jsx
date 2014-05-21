c5df3dabe772eae45818d287bee0deb5d3dedeb3
Miro Nieminen <miro.nieminen@futurice.com>
2014-05-21 20:39:35 +0300

    Add link to specific Event-page

--- app/jsx/components/Event.jsx
+++ app/jsx/components/Event.jsx
@@ -7,14 +7,18 @@ define([
 ) {
     var Event = React.createClass({
         render: function() {
+
             return (
                 <div className={"event"}>
                     <h3 className={"name"}>
-                        {this.props.model.get('Name')}
+                        {this.props.model.get('name')}
                     </h3>
                     <p className={"description"}>
-                        {this.props.model.get('Description')}
+                        {this.props.model.get('description')}
                     </p>
+                    <a href={"#event/" + this.props.model.get('id')}>
+                        More info
+                    </a>
                 </div>
             );
         }
