ad069fa9d600a9a16ba6b7edb973d683b3240183
TPihko <tpihko@gmail.com>
2014-05-25 22:58:49 +0300

    Redeem reservation / cancel reservation redemption

--- app/jsx/pages/Reservation.jsx
+++ app/jsx/pages/Reservation.jsx
@@ -22,6 +22,18 @@ define([
         updateOnProps: {
             'model': 'model'
          },
+
+        redeemReservation: function() {
+            this.props.model.set({'redeemed':'true'});
+            this.props.model.save();
+            console.log("Redeemed reservation");
+        },
+
+        cancelRedemption: function() {
+            this.props.model.set({'redeemed':'false'});
+            this.props.model.save();
+            console.log("Cancelled redemption");
+        },
         
         render: function() {
 
@@ -30,10 +42,16 @@ define([
                     ID:  {this.props.model.get('id')}
                 </h3>
                 <p className={"expired"}>
-                    Expired: {this.props.model.get('expired') ? 'YES' : 'NO'}
+                    {!this.props.model.get('expired') ? 
+                        'Reservation expired' : 
+                        'Reservation expires dd.mm.yyyy'
+                    }
                 </p>
                 <p className={"redeemed"}>
-                    Redeemed: {this.props.model.get('redeemed') ? 'YES' : 'NO'}
+                    {this.props.model.get('redeemed') ? 
+                        <button className="button" onClick={this.cancelRedemption}>Cancel redemption</button> :
+                        <button className="button" onClick={this.redeemReservation}>Redeem reservation</button>                        
+                    }
                 </p>
             </div>
         }
