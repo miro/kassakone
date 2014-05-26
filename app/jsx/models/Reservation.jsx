fad3ecf167ff0a83fda9498aa0e527f958fd5ef4
Miro Nieminen <miro.nieminen@futurice.com>
2014-05-26 18:46:52 +0300

    Remove unnecessary default values for model

--- app/jsx/models/Reservation.jsx
+++ app/jsx/models/Reservation.jsx
@@ -9,16 +9,6 @@ define([
 ) {
 
     return Backbone.Model.extend({
-        defaults: {
-            'id': null,
-            'expired': null,
-            'redeemed': null
-            /*'ID': null,
-            'EventOccurancesID': null,
-            'Name': null,
-            'Amount': null*/
-        },
-
         url: function() {
             return config.baseUrl + '/Reservation/' + (!_.isUndefined(this.id) ? this.id : '');
         }
