4bdbd31d3a9d7e7e66065335ea2343d7b74be649
Miro Nieminen <miro.nieminen@futurice.com>
2014-05-27 21:20:06 +0300

    Add basic version of selling ticket to occurrence

--- app/jsx/pages/EventOccurrencePage.jsx
+++ app/jsx/pages/EventOccurrencePage.jsx
@@ -4,12 +4,14 @@ define([
     'underscore',
     'react',
     'react-backbone',
-    'components/Reservation'
+    'components/Reservation',
+    'models/Reservation'
 ], function(
     _,
     React,
     rbbMixin,
-    ReservationComponent
+    ReservationComponent,
+    ReservationModel
 ) {
 
     var EventOccurrencePage = React.createClass({
@@ -20,11 +22,23 @@ define([
             occurrence: 'model',
             reservations: 'collection'
         },
+
+        sellTicket: function sellTicket() {
+            var self = this;
+            var reservationModel = new ReservationModel();
+            reservationModel.occurrenceId = this.props.occurrence.get('id');
+            reservationModel.save({
+                success: function(model) {
+                    self.props.reservations.add(this);
+                }
+            });
+            this.props.reservations.add(reservationModel);
+        },
         
         render: function() {
             var reservations = [];
             _.each(this.props.reservations.models, function(reservation) {
-                reservations.push(<ReservationComponent model={reservation} />);
+                reservations.push(<ReservationComponent model={reservation} key={reservation.get('id')} />);
             });
 
             return <div className="occurrence-page">
@@ -32,14 +46,21 @@ define([
                     {moment(this.props.occurrence.get('startTime')).format('DD.MM.YY HH:mm')}
                 </h3>
 
-                <div className="info">
-                    <p className="price">
-                        {this.props.occurrence.get('price')} &euro;
-                    </p>
-                    <p className="status">
-                        Tickets reserved {this.props.occurrence.get('reservedPlaces')} + {this.props.occurrence.get('totalPlaces')} / {this.props.occurrence.get('soldPlaces')}
-                    </p>
+                <div className="meta clearfix">
+                    <div className="info">
+                        <p className="price">
+                            {this.props.occurrence.get('price')} &euro;
+                        </p>
+
+                        <p className="status">
+                            Tickets reserved {this.props.occurrence.get('reservedPlaces')} + {this.props.occurrence.get('totalPlaces')} / {this.props.occurrence.get('soldPlaces')}
+                        </p>
+                    </div>
+
+                    <button id="sellticket" onClick={this.sellTicket}>Sell Ticket</button>
                 </div>
+
+                
 
 
                 <h4 className="title">Reservations</h4>
